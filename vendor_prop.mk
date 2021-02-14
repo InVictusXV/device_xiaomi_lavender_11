3c98c1a81539f8bdfaec7034f1dad52557417ff9
InVictusXV <47210177+InVictusXV@users.noreply.github.com>
2021-02-14 09:20:37 +0700

    lavender: add props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -242,18 +242,24 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_backpressure=0 \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.early_phase_offset_ns=11600000 \
+    debug.sf.early_app_phase_offset_ns=11600000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.sdm.support_writeback=0 \
     debug.sf.enable_hwc_vds=0 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
     debug.cpurend.vsync=true
+    
+# The default sf phase offset is set to 6ms, to avoid it be included into next
+# vsync threshold, set high fps early sf and next vsync threshold phase offset
+# to 6.1ms, which is bigger than all sf phase offsets in normal frame rate.
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.high_fps_early_phase_offset_ns=6100000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
