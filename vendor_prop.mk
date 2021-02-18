669ed42a220a0bfafa448fe25de7fe2aa2f1fda5
InVictusXV <47210177+InVictusXV@users.noreply.github.com>
2021-02-18 16:48:48 +0700

    lavender: updated props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -150,15 +150,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.latch_unsignaled=1 \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.hw=1 \
-    debug.sf.disable_backpressure=1 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.cpurend.vsync=true \
     debug.hwui.use_buffer_age=false \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
