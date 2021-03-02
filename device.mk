046143e0429848f216cf22a29c59e33bb4facb0c
aradhearizz <aradheakiki123@gmail.com>
2021-03-02 06:18:06 +0000

    lavender: Don't build tinymix

--- device.mk
+++ device.mk
@@ -44,8 +44,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    tinymix
+    libvolumelistener 
 
 PRODUCT_PACKAGES += \
     libsndmonitor \
