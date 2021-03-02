44cb9e0e91f2fd0fe3691fe7aae36a2d4f355995
aradhearizz <aradheakiki123@gmail.com>
2021-03-02 05:50:34 +0000

    lavender: Uprev vendor.display.config to 2.0

--- device.mk
+++ device.mk
@@ -134,8 +134,8 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     memtrack.sdm660 \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9_vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0_vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service \
